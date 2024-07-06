cacd8ab085dd1033dc1caa08ba16041d1a0ac134
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-06 17:18:54 -0400

    Initial Commit

---
+++ Slzzp/SlzzpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlzzpApp.swift
+//  Slzzp
+//
+//  Created by Parth Antala on 2024-07-06.
+//
+
+import SwiftUI
+
+@main
+struct SlzzpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
